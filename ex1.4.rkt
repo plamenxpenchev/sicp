aa5d474a725c9534f4898b90393b7a3b92b6b407
Plamen Penchev <plamenxpenchev@gmail.com>
2024-05-20 07:48:07 +0300

    Additional examples 1.4

--- ex1.4.rkt
+++ ex1.4.rkt
@@ -3,3 +3,11 @@
 (a-plus-abs-b 1 2)
 (a-plus-abs-b 1 0)
 (a-plus-abs-b 1 -1)
+
+(define (abs x)((if (> x 0) + -) x))
+(define (my-a-plus-abs-b a b)(+ a (abs b)))
+(my-a-plus-abs-b 1 2)
+(my-a-plus-abs-b 1 0)
+(my-a-plus-abs-b 1 -1)
+
+(abs -77)
