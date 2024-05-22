5eb741314e726b1d9d52dac7797dd6f0da4b181d
Plamen Penchev <plamenxpenchev@gmail.com>
2024-05-22 07:45:02 +0300

    Allow x to be a free variable in internal definitions
    Lexical scoping

--- sqrt-with-internal-definitions.rkt
+++ sqrt-with-internal-definitions.rkt
@@ -6,13 +6,15 @@
   (/ (+ x y) 2))
 
 (define (sqrt x)
-  (define (good-enough? guess x)
+  (define (good-enough? guess)
     (< (abs (- (square guess) x)) 0.001))
-  (define (improve guess x)
+  (define (improve guess)
     (average guess (/ x guess)))
-  (define (sqrt-iter guess x)
-    (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
-  (sqrt-iter 1.0 x))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
 
 (sqrt 9)
 (sqrt 4)
